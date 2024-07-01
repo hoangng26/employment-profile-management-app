e3fc9171ab66b73b8a0899004eaea711488d3114
Nguyen Le Huu Hoang <contact.hoangng26@gmail.com>
2024-07-01 14:46:01 +0700

    Update database.config.ts

--- backend/src/core/database/database.config.ts
+++ backend/src/core/database/database.config.ts
@@ -2,5 +2,5 @@ import { SequelizeOptions } from 'sequelize-typescript';
 
 export const databaseConfig: SequelizeOptions = {
   dialect: 'sqlite',
-  storage: './src/core/database/database.db',
+  storage: './database.db',
 };
